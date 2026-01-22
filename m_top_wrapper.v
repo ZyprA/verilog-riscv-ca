2111e00c13b24fdd54d954687f2c97c212abdcbb
ZyprA <devkookey@gmail.com>
2026-01-22 15:03:14 +0900

    fix r_cc init

--- m_top_wrapper.v
+++ m_top_wrapper.v
@@ -4,7 +4,7 @@
 
 module m_top_wrapper();
     reg r_clk = 0; initial #150 forever #50 r_clk = ~r_clk;
-    reg [31:0] r_cc;
+    reg [31:0] r_cc = 0;
     always @(posedge r_clk) r_cc <= r_cc + 1;
     initial #1000000 begin
         $display("timeout");
