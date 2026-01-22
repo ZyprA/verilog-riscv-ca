2325af6b3733256318d9c3b2bf0f9985c2d0c2bc
ZyprA <devkookey@gmail.com>
2026-01-22 17:48:08 +0900

    add bypass

--- m_RF.v
+++ m_RF.v
@@ -6,8 +6,10 @@ module m_RF(w_clk, w_ra1, w_ra2, w_rd1, w_rd2, w_wa, w_we, w_wd);
     input wire [31:0] w_wd;
     output wire [31:0] w_rd1, w_rd2;
     reg [31:0] mem [31:0];
-    assign w_rd1 = (w_ra1 == 5'd0) ? 32'd0 : mem[w_ra1];
-    assign w_rd2 = (w_ra2 == 5'd0) ? 32'd0 : mem[w_ra2];
+    wire bp1 = (w_we & w_ra1==w_wa);
+    wire bp2 = (w_we & w_ra2==w_wa);
+    assign w_rd1 = (w_ra1 == 5'd0) ? 32'd0 : (w_bp1) ? w_wd : mem[w_ra1];
+    assign w_rd2 = (w_ra2 == 5'd0) ? 32'd0 : (w_bp2) ? w_wd : mem[w_ra2];
     always @(posedge w_clk) if (w_we) mem[w_wa] <= w_wd;
     always @(posedge w_clk) if (w_we & w_wa==5'd30) $finish; // x30に書き込みをすると終了する
     integer i; initial for (i=0; i<32; i=i+1) mem[i] = 0;
